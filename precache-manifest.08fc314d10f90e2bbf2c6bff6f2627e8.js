4a6d728aa7883fef26ed4339acf1d5c0e61bff8f
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-08-13 01:05:36 +0100

    Updates

---
+++ precache-manifest.08fc314d10f90e2bbf2c6bff6f2627e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faee678ed63f5908efa3e707ef1cc4d7",
+    "url": "/React-Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "a335fcc5ba311312affc",
+    "url": "/React-Monsters-Rolodex/static/css/main.e3e7e2f5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/React-Monsters-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Monsters-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a335fcc5ba311312affc",
+    "url": "/React-Monsters-Rolodex/static/js/main.521f0505.chunk.js"
+  },
+  {
+    "revision": "026ce6368fc2823e93f3",
+    "url": "/React-Monsters-Rolodex/static/js/runtime-main.a3fae53e.js"
+  }
+]);
